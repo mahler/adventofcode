b615ec22b8d3f3851f6db2a9fcb5d907b7e7159f
Flemming Mahler <flemming@mahler.io>
2020-12-20 19:02:56 +0100

    Task and solution for day 4

---
+++ 2015/04/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	input := "iwrupvqb"
+	counter := 0
+
+	fmt.Println()
+	fmt.Print("Day 04, Part 1: The Ideal Stocking Stuffer")
+	for {
+		data := input + strconv.Itoa(counter)
+		sum := md5.Sum([]byte(data))
+		str := hex.EncodeToString(sum[:])
+		if str[0:5] == "00000" {
+			// AdventCoin found.
+			fmt.Println(counter)
+			break
+		}
+		counter++
+	}
+	fmt.Println("lowest positive number - zero5:", counter)
+
+	fmt.Println()
+	fmt.Print("Day 04, Part 2:")
+	// reset counter for good measure.
+	counter = 0
+	for {
+		data := input + strconv.Itoa(counter)
+		sum := md5.Sum([]byte(data))
+		str := hex.EncodeToString(sum[:])
+		if str[0:6] == "000000" {
+			// AdventCoin found.
+			fmt.Println(counter)
+			break
+		}
+		counter++
+	}
+	fmt.Println("lowest positive number - zero6:", counter)
+
+}

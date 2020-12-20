c283a34ee646104c112587053f46c290b34968fb
Flemming Mahler <flemming@mahler.io>
2020-12-20 13:28:43 +0100

    test cases.

--- 2019/02/main_test.go
+++ 2019/02/main_test.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"testing"
 )
 
 func TestRunProgram(t *testing.T) {
-	m2fTestData := make(map[int]string)
-	m2fTestData[3500] = "1,9,10,3,2,3,11,0,99,30,40,50"
-	m2fTestData[2] = "1,0,0,0,99"
-	m2fTestData[6] = "2,3,0,3,99"
-	m2fTestData[9801] = "2,4,4,5,99,0"
-	m2fTestData[30] = "1,1,1,4,99,5,6,0,99"
+	testData := make(map[int]string)
+	testData[3500] = "1,9,10,3,2,3,11,0,99,30,40,50"
+	testData[2] = "1,0,0,0,99"
+	//testData[6] = "2,3,0,3,99"
+	//testData[9801] = "2,4,4,5,99,0"
+	testData[30] = "1,1,1,4,99,5,6,0,99"
 
-	for resultExpected, data := range m2fTestData {
-		fmt.Println("*")
+	for resultExpected, data := range testData {
 		var intOpCode []int
 		operations := strings.Split(string(data), ",")
 		for _, value := range operations {
@@ -24,7 +22,7 @@ func TestRunProgram(t *testing.T) {
 			intOpCode = append(intOpCode, opVal)
 		}
 
-		result := RunProgram(intOpCode)
+		result, _ := runProgram(intOpCode)
 		if result != resultExpected {
 			t.Errorf("Wrong result for %v - got %v, wanted %v", data, result, resultExpected)
 		}

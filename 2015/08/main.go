8c5115521ca3379c6860574dc5b3700eb411adca
Flemming Mahler <flemming@mahler.io>
2020-12-22 18:11:38 +0100

    Task, input and solution for day 8

---
+++ 2015/08/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	fileContent, err := ioutil.ReadFile("puzzle.txt")
+	if err != nil {
+		log.Fatal("File reading error", err)
+
+	}
+	lines := strings.Split(strings.TrimSpace(string(fileContent)), "\n")
+	fmt.Println("Instructions in dataset:", len(lines))
+	//	fmt.Println("lines", len(lines))
+	sum := 0
+	for _, line := range lines {
+		re := regexp.MustCompile("(\\\\\\\\)|(\\\\\\\")|(\\\\x[0-9a-f]{2})")
+		matches := re.FindAllStringSubmatch(line, -1)
+		sum += 2
+		count := len(matches)
+		for i := 0; i < count; i++ {
+			sum += len(matches[i][0]) - 1
+		}
+	}
+	fmt.Println()
+	fmt.Println("Day 8: Matchsticks")
+	fmt.Println("Total for the entire file:", sum)
+
+	fmt.Println()
+	fmt.Println("Part 2")
+	sum = 0
+	for _, line := range lines {
+		re1 := regexp.MustCompile("\\\\")
+		new := re1.ReplaceAllLiteralString(line, "\\\\")
+
+		re2 := regexp.MustCompile("\\\"")
+		new = re2.ReplaceAllLiteralString(new, "\\\"")
+		sum += 2 + len(new) - len(line)
+	}
+	fmt.Println(sum)
+
+}

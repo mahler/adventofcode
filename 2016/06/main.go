de80a2f4c75c6c2390f26300c6a16ca0b0c7a088
Flemming Mahler <flemming@mahler.io>
2020-12-29 22:15:45 +0100

    Challenge description and solution for part 2 of day 2016-06

--- 2016/06/main.go
+++ 2016/06/main.go
@@ -35,14 +35,39 @@ func main() {
 		}
 
 		// Sort the map by value (letter occurence)
-		rankedLetterCount := rankByCount(counterMap)
+		rankedLetterCount := rankByCountDesc(counterMap)
 
 		codeWord += rankedLetterCount[0].Key
 	}
-	fmt.Println("The secret message:", codeWord)
+	fmt.Println("The secret message (desc):", codeWord)
+	//----------------------------------------------
+	fmt.Println()
+	fmt.Println("Part 2")
+	// Reset code word
+	codeWord = ""
+	for charNum := 0; charNum < len(fileRows[0]); charNum++ {
+		counterMap := make(map[string]int)
+		for _, fileRow := range fileRows {
+			char := fileRow[charNum : charNum+1]
+
+			if _, ok := counterMap[char]; !ok {
+				counterMap[char] = 1
+			} else {
+				counterMap[char]++
+			}
+
+		}
+
+		// Sort the map by value (letter occurence)
+		rankedLetterCount := rankByCountAsc(counterMap)
+
+		codeWord += rankedLetterCount[0].Key
+	}
+	fmt.Println("The secret message (asc):", codeWord)
+
 }
 
-func rankByCount(counterMap map[string]int) PairList {
+func rankByCountDesc(counterMap map[string]int) PairList {
 	pl := make(PairList, len(counterMap))
 	i := 0
 	for k, v := range counterMap {
@@ -50,6 +75,17 @@ func rankByCount(counterMap map[string]int) PairList {
 		i++
 	}
 	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+func rankByCountAsc(counterMap map[string]int) PairList {
+	pl := make(PairList, len(counterMap))
+	i := 0
+	for k, v := range counterMap {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(pl)
 	return pl
 }
 

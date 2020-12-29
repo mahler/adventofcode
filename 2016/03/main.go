95ee2daebb27106c23f6814f1daa302f1dfe786b
Flemming Mahler <flemming@mahler.io>
2020-12-29 10:59:29 +0100

    Solution to part 2 and challenge text for day 2016-03

--- 2016/03/main.go
+++ 2016/03/main.go
@@ -38,4 +38,34 @@ func main() {
 	fmt.Println("how many of the listed triangles are possible?")
 	fmt.Println(validTriangles)
 	// ----------------------------------------
+	fmt.Println()
+	fmt.Println("Part 2: Vertical Triangles")
+
+	// Reset reused vars
+	validTriangles = 0
+
+	// Build triangle data 2D slice
+	triData := make([][]int, len(fileRows))
+	for i, fileRow := range fileRows {
+		fields := strings.Fields(fileRow)
+
+		sideOne, _ := strconv.Atoi(fields[0])
+		sideTwo, _ := strconv.Atoi(fields[1])
+		sideThree, _ := strconv.Atoi(fields[2])
+
+		dataLine := []int{sideOne, sideTwo, sideThree}
+		triData[i] = dataLine
+	}
+
+	// Walk the triangle data for vertical triangles
+	for t := 0; t < len(fileRows); t += 3 {
+		for u := 0; u < 3; u++ {
+			if triData[t][u]+triData[t+1][u] > triData[t+2][u] && triData[t+1][u]+triData[t+2][u] > triData[t][u] && triData[t][u]+triData[t+2][u] > triData[t+1][u] {
+				validTriangles++
+			}
+			//fmt.Println(t+u, "#", triData[t][u], "-", triData[t+1][u], "-", triData[t+2][u])
+		}
+	}
+	fmt.Println("How many of the listed vertical triangles are possible?")
+	fmt.Println(validTriangles)
 }

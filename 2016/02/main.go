14eaae804ed837c1098058a149ba269fe1889969
Flemming Mahler <flemming@mahler.io>
2020-12-29 10:06:12 +0100

    Solution for part2 of day 2016-02

--- 2016/02/main.go
+++ 2016/02/main.go
@@ -21,7 +21,7 @@ func main() {
 	fileRows := strings.Split(string(fileContent), "\n")
 
 	// Keypad
-	keys := [3][3]string{
+	keypad := [3][3]string{
 		{"1", "2", "3"},
 		{"4", "5", "6"},
 		{"7", "8", "9"},
@@ -60,7 +60,66 @@ func main() {
 				yPos = 2
 			}
 		}
-		pincode += keys[yPos][xPos]
+		pincode += keypad[yPos][xPos]
 	}
-	fmt.Println("What is the bathroom code?", pincode)
+	fmt.Println("What is the bathroom code?")
+	fmt.Println(pincode)
+	// Part 2 ---------------------------------------------
+	fmt.Println()
+	fmt.Println("Part 2")
+
+	// Reset reused vars for part 2
+	pincode = ""
+	xPos = 0
+	yPos = 2
+
+	// setup keypad
+	p2keypad := [5][5]string{
+		{"", "", "1", "", ""},
+		{"", "2", "3", "4", ""},
+		{"5", "6", "7", "8", "9"},
+		{"", "A", "B", "C", ""},
+		{"", "", "D", "", ""},
+	}
+
+	for _, instructions := range fileRows {
+		for i := 0; i < len(instructions); i++ {
+			lastX := xPos
+			lastY := yPos
+			switch instructions[i : i+1] {
+			case "U":
+				yPos--
+			case "D":
+				yPos++
+			case "L":
+				xPos--
+			case "R":
+				xPos++
+			}
+
+			// Handle border overruns
+			if xPos < 0 {
+				xPos = 0
+			} else if xPos > 4 {
+				xPos = 4
+			}
+
+			if yPos < 0 {
+				yPos = 0
+			} else if yPos > 4 {
+				yPos = 4
+			}
+
+			// If outside keypad keys, step back
+			if p2keypad[yPos][xPos] == "" {
+				xPos = lastX
+				yPos = lastY
+			}
+		}
+		pincode += p2keypad[yPos][xPos]
+	}
+
+	fmt.Println("what is the correct bathroom code with new keypad?")
+	fmt.Println(pincode)
+
 }

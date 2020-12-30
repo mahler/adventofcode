16669c895093abd4a6ca2b603d7acb61b961c428
Flemming Mahler <flemming@mahler.io>
2020-12-30 11:39:56 +0100

    Solution for part 2

--- 2016/07/main.go
+++ 2016/07/main.go
@@ -25,18 +25,33 @@ func main() {
 		return
 	}
 
+	// Build ipTable to reuse data in part2
+	ipTable := []ipv7{}
+
 	fileRows := strings.Split(string(fileContent), "\n")
 	for _, ip7string := range fileRows {
 
 		ip := convertStringToIP(ip7string)
+		ipTable = append(ipTable, ip)
 		if abbaSupport(ip) {
 			ipCounter++
 		}
 	}
-	fmt.Println("How many IPs in your puzzle input support TLS?")
+	fmt.Println("How many IPs in your puzzle input support SSL?")
 	fmt.Println(ipCounter)
 	// -----------------------------------
 
+	fmt.Println()
+	fmt.Println("Part 2: SSL support")
+	sslCount := 0
+	for _, ip := range ipTable {
+		if sslSupport(ip) {
+			sslCount++
+		}
+	}
+
+	fmt.Println("How many IPs in your puzzle input support SSL?")
+	fmt.Println(sslCount)
 }
 
 func convertStringToIP(s string) ipv7 {
@@ -56,6 +71,7 @@ func convertStringToIP(s string) ipv7 {
 	return ip
 }
 
+// ---------- ABBA for Part 1
 func abbaSupport(ip ipv7) bool {
 	for _, hyper := range ip.hypernet {
 		if abbaCheck(hyper) {
@@ -82,4 +98,38 @@ func abbaCheck(s string) bool {
 
 	// abba sequence not found
 	return false
+}
+
+// ---------- SSL for Part 2
+func sslSupport(ip ipv7) bool {
+	// Find all abas in support in supernets
+	abas := []string{}
+
+	for _, snet := range ip.supernet {
+		abas = append(abas, abaSplit(snet)...)
+	}
+
+	// ABAs found - check for BABs in hypernets
+	for _, aba := range abas {
+		bab := aba[1:2] + aba[0:1] + aba[1:2]
+		for _, hnet := range ip.hypernet {
+			if strings.Index(hnet, bab) > -1 {
+				return true
+			}
+		}
+	}
+
+	// No BAB found in hypernet, check failed.
+	return false
+}
+
+func abaSplit(supernet string) []string {
+	abas := []string{}
+	for i := 0; i < len(supernet)-2; i++ {
+		// ABA, but not AAA
+		if supernet[i] == supernet[i+2] && supernet[i] != supernet[i+1] {
+			abas = append(abas, supernet[i:i+3])
+		}
+	}
+	return abas
 }

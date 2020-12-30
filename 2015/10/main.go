3ead504afe95b669068d9bc1bd59804fb6668bbf
Flemming Mahler <flemming@mahler.io>
2020-12-30 12:42:41 +0100

    Solution for 2015-10 part 2

--- 2015/10/main.go
+++ 2015/10/main.go
@@ -16,9 +16,22 @@ func main() {
 	result := input
 	for i := 0; i < 40; i++ {
 		result = lookAndSay(result)
+		//fmt.Println("Round ", i+1)
 	}
 
 	fmt.Println("Look&Say 40 rounds:")
+	fmt.Println(len(result))
+
+	//------------ Part 2
+	fmt.Println("Part 2 - 50 rounds")
+
+	byteResult := []byte(input)
+	for i := 0; i < 50; i++ {
+		byteResult = lookAndSayTurbo(byteResult)
+		//fmt.Println("Round ", i+1)
+	}
+	fmt.Println("Look&Say 50 rounds:")
+	result = string(byteResult)
 	fmt.Println(len(result))
 }
 
@@ -43,4 +56,22 @@ func lookAndSay(inputstring string) string {
 	returnString += strconv.Itoa(count) + string(current)
 
 	return returnString
+}
+
+// Function from https://github.com/mevdschee/AdventOfCode2015/blob/master/day10/part2.go
+// Which runs much faster
+func lookAndSayTurbo(str []byte) []byte {
+	ch := str[0]
+	count := 0
+	result := []byte{}
+	for i := 0; i < len(str); i++ {
+		if str[i] != ch {
+			result = append(result, byte(48+count), ch)
+			ch = str[i]
+			count = 0
+		}
+		count++
+	}
+	result = append(result, byte(48+count), ch)
+	return result
 }

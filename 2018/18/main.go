ff5014e2c07f7b16767ff85bf73ef7af4d1f42cc
Flemming Mahler <flemming@mahler.io>
2021-01-22 18:15:01 +0100

    Step 1 of solution - can run sample setup

--- 2018/18/main.go
+++ 2018/18/main.go
@@ -11,6 +11,12 @@ import (
 // | - Tree
 // # - Lumberyard
 
+type pointCount struct {
+	ground int
+	tree   int
+	lumber int
+}
+
 // NPmap defined to allow map to be passed to function(s)
 type NPmap [][]string
 
@@ -37,10 +43,14 @@ func main() {
 		northpolemap = append(northpolemap, mapRow)
 	}
 
+	fmt.Println("Round 0 /")
 	printMap(northpolemap)
-	runRound(&northpolemap)
-	printMap(northpolemap)
-	//	test(&northpolemap)
+	for round := 1; round <= 10; round++ {
+		runRound(&northpolemap)
+		fmt.Println("Round", round, "/")
+		printMap(northpolemap)
+	}
+
 }
 
 func printMap(npmap NPmap) {
@@ -53,58 +63,66 @@ func printMap(npmap NPmap) {
 	}
 }
 
-func test(npmap *NPmap) {
-	for x := range *npmap {
-		fmt.Println((*npmap)[x])
+func runRound(npmap *NPmap) {
 
+	duplicate := make(NPmap, len((*npmap)))
+	for i := range *npmap {
+		duplicate[i] = make([]string, len((*npmap)[i]))
+		copy(duplicate[i], (*npmap)[i])
+	}
+
+	for row := range duplicate {
+		for col := range duplicate[row] {
+			p := mapPoint(duplicate, row, col)
+			switch (*npmap)[row][col] {
+			case "#": // An acre containing a lumberyard will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees.
+				// Otherwise, it becomes open.
+				if p.lumber > 0 && p.tree > 0 {
+					(*npmap)[row][col] = "#"
+				} else {
+					(*npmap)[row][col] = "."
+				}
+
+			case "|": // An acre filled with trees will become a lumberyard if three or more adjacent acres were lumberyards. Otherwise, nothing happens.
+				if p.lumber > 2 {
+					(*npmap)[row][col] = "#"
+				}
+			case ".": // An open acre will become filled with trees if three or more adjacent acres contained trees. Otherwise, nothing happens.
+				if p.tree > 2 {
+					(*npmap)[row][col] = "|"
+				}
+			}
+		}
 	}
 }
 
-func runRound(npmap *NPmap) {
-	for row := range *npmap {
-		for col := range (*npmap)[row] {
-			switch (*npmap)[row][col] {
-			case ".":
-				treeCount := 0
-				// An open acre will become filled with trees if three or more adjacent acres contained trees. Otherwise, nothing happens.
-				if row-1 >= 0 { // If no row above
-					if col-1 >= 0 && (*npmap)[row-1][col-1] == "|" { // Not beyond map border left.
-						treeCount++
+func mapPoint(myMap NPmap, myRow, myCol int) pointCount {
+	var p pointCount
+	for row := -1; row < 2; row++ {
+		for col := -1; col < 2; col++ {
+			thisRow := myRow + row
+			thisCol := myCol + col
+			//fmt.Println("row:", thisRow, "- col:", thisCol)
+			// If not the mappoint itself.
+			if !(col == 0 && row == 0) {
+				// If not above the top row or below the buttom row.
+				if thisRow >= 0 && thisRow <= len(myMap)-1 {
+					// If not too far to the left nor to the right on the map...
+					if thisCol >= 0 && thisCol <= len(myMap[thisRow])-1 {
+						//fmt.Println("Found:", myMap[thisRow][thisCol])
+						switch myMap[thisRow][thisCol] {
+						case "#":
+							p.lumber++
+						case "|":
+							p.tree++
+						case ".":
+							p.ground++
+						}
 					}
-					if (*npmap)[row-1][col] == "|" {
-						treeCount++
-					}
-					if col+1 < len((*npmap)[row]) && (*npmap)[row-1][col+1] == "|" { // Not beyond map border right.
-						treeCount++
-					}
-				} // row less than 0, nothing here...
-				if col-1 >= 0 && (*npmap)[row][col-1] == "|" {
-					treeCount++
-				}
-				if col+1 > len((*npmap)[row]) && (*npmap)[row][col+1] == "|" {
-					treeCount++
-				}
-				if row+1 < len((*npmap)) {
-					if col-1 >= 0 && (*npmap)[row+1][col-1] == "|" { // Not beyond map border left.
-						treeCount++
-					}
-					if (*npmap)[row+1][col] == "|" {
-						treeCount++
-					}
-					if col+1 < len((*npmap)[row]) && (*npmap)[row+1][col+1] == "|" { // Not beyond map border right.
-						treeCount++
-					}
-				}
-				if treeCount > 2 {
-					(*npmap)[row][col] = "|"
-				}
-			case "|":
-				// An acre filled with trees will become a lumberyard if three or more adjacent acres were lumberyards. Otherwise, nothing happens.
 
-			case "#":
-				// An acre containing a lumberyard will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees. Otherwise, it becomes open.
+				}
 			}
 		}
-		fmt.Println()
 	}
+	return p
 }

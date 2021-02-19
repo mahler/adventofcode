25cfb71bff353b98da60de12df5ddd02d86bff6e
Flemming Mahler <flemming@mahler.io>
2021-02-19 16:14:13 +0100

    Shell script to run all days

---
+++ 2020/runAll.sh
@@ -0,0 +1,125 @@
+cd 01
+echo "---------- 01 ----------"
+go run main.go
+cd ..
+
+cd 02
+echo "---------- 02 ----------"
+go run main.go
+cd ..
+
+cd 03
+echo "---------- 03 ----------"
+go run main.go
+cd ..
+
+cd 04
+echo "---------- 04 ----------"
+go run main.go
+cd ..
+
+cd 05
+echo "---------- 05 ----------"
+go run main.go
+cd ..
+
+cd 06
+echo "---------- 06 ----------"
+go run main.go
+cd ..
+
+cd 07
+echo "---------- 07 ----------"
+go run main.go
+cd ..
+
+cd 08
+echo "---------- 08 ----------"
+go run main.go
+cd ..
+
+cd 09
+echo "---------- 09 ----------"
+go run main.go
+cd ..
+
+cd 10
+echo "---------- 10 ----------"
+go run main.go
+cd ..
+
+cd 11
+echo "---------- 11 ----------"
+go run main.go
+cd ..
+
+cd 12
+echo "---------- 12 ----------"
+go run main.go
+cd ..
+
+cd 13
+echo "---------- 13 ----------"
+go run main.go
+cd ..
+
+cd 14
+echo "---------- 14 ----------"
+go run main.go
+cd ..
+
+cd 15
+echo "---------- 15 ----------"
+go run main.go
+cd ..
+
+cd 16
+echo "---------- 16 ----------"
+go run main.go
+cd ..
+
+cd 17
+echo "---------- 17 ----------"
+go run main.go
+cd ..
+
+cd 18
+echo "---------- 18 ----------"
+go run main.go
+cd ..
+
+cd 19
+echo "---------- 19 ----------"
+go run main.go
+cd ..
+
+cd 20
+echo "---------- 20 ----------"
+go run main.go
+cd ..
+
+cd 21
+echo "---------- 21 ----------"
+go run main.go
+cd ..
+
+cd 22
+echo "---------- 22 ----------"
+go run main.go
+cd ..
+
+cd 23
+echo "---------- 23 ----------"
+go run main.go
+cd ..
+
+cd 24
+echo "---------- 24 ----------"
+go run main.go
+cd ..
+
+cd 25
+echo "---------- 25 ----------"
+go run main.go
+cd ..
+

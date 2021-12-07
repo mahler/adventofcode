52e02965e2493a2324563a6aacac22d4a62c1d5f
Flemming Mahler <flemming@mahler.io>
2021-12-07 16:47:00 +0100

    Solved part 2

--- 2017/01/main.go
+++ 2017/01/main.go
@@ -25,17 +25,35 @@ func main() {
 
 	fmt.Println()
 	fmt.Println("2017 - Day 01 Part 1")
-	fmt.Println(inverseCaptcha(dataStr, 1))
+	fmt.Println(inverseCaptcha(dataStr))
 
+	//-------------------------------------
+	fmt.Println()
+	fmt.Println("Part 2")
+	fmt.Println(part2Captcha(dataStr))
 }
 
-func inverseCaptcha(line string, offset int) int {
+func inverseCaptcha(line string) int {
 	sum := 0
+	offset := 1
 	length := len(line)
 	for i := 0; i < length; i++ {
 		if line[i] == line[(i+offset)%length] {
 			tmp, _ := strconv.Atoi(line[i : i+1])
 			sum += tmp
+		}
+	}
+	return sum
+}
+
+func part2Captcha(line string) int {
+	sum := 0
+	a := line[:len(line)/2]
+	b := line[len(line)/2:]
+	for i := 0; i < len(a); i++ {
+		if a[i] == b[i] {
+			tmp, _ := strconv.Atoi(string(a[i]))
+			sum += tmp * 2
 		}
 	}
 	return sum

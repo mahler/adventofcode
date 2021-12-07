e6ddc069be3b124cedb48872818f1e4451f05189
Flemming Mahler <flemming@mahler.io>
2021-12-07 08:51:02 +0100

    Solved part 1

---
+++ 2021/07/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fileContent, err := os.ReadFile("puzzle.txt")
+	if err != nil {
+		log.Fatal("File reading error", err)
+		return
+	}
+
+	// Setup
+	source := string(fileContent)
+	strCrabSubmarines := strings.Split(source, ",")
+
+	intCrabSubmarines := []int{}
+	for i := 0; i < len(strCrabSubmarines); i++ {
+		cSubmarine, _ := strconv.Atoi(strCrabSubmarines[i])
+		intCrabSubmarines = append(intCrabSubmarines, cSubmarine)
+	}
+
+	// Find min and max pos for testing...
+	minPos := 9999
+	maxPos := -1000
+
+	for _, crabPos := range intCrabSubmarines {
+		if crabPos < minPos {
+			minPos = crabPos
+		}
+		if crabPos > maxPos {
+			maxPos = crabPos
+		}
+	}
+
+	//	fmt.Println("Min/max pos:", minPos, "/", maxPos)
+
+	// Test positions
+	fuelUsage := 99999999
+	superPos := 0
+
+	for pos := minPos; pos < maxPos+1; pos++ {
+		crabFuel := 0
+		for _, crabSubPos := range intCrabSubmarines {
+			crabFuel += diff(crabSubPos, pos)
+		}
+
+		if crabFuel < fuelUsage {
+			superPos = pos
+			fuelUsage = crabFuel
+		}
+	}
+
+	fmt.Println("Part 01/")
+	fmt.Println("Position:", superPos)
+	fmt.Println("Fuel usage", fuelUsage)
+
+}
+
+func diff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}

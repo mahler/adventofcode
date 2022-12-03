667301e62652b7d8c7c6b2bbbfe9377a0a1afeb4
Flemming Mahler <flemming@mahler.io>
2022-12-03 18:11:13 +0100

    Solution for 2022-03 part 1 & 2

---
+++ 2022/03/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("puzzle.txt")
+	if err != nil {
+		log.Fatal("File reading error", err)
+	}
+	fileLines := strings.Split(strings.TrimSpace(string(data)), "\n")
+
+	total := 0
+	for _, rucksack := range fileLines {
+		set := make(map[rune]bool)
+		l := len(rucksack)
+		first, last := rucksack[:l/2], rucksack[l/2:]
+		for _, char := range first {
+			set[char] = true
+		}
+		for _, char := range last {
+			if _, ok := set[char]; ok {
+				total += getPrio(char)
+				break
+			}
+		}
+	}
+	fmt.Println("Part 1:")
+	fmt.Println(total)
+	fmt.Println()
+	// Part 2
+	total = 0
+
+	for i := 0; i < len(fileLines); i += 3 {
+		a, b, c := fileLines[i], fileLines[i+1], fileLines[i+2]
+		seenA := make(map[rune]bool)
+		seenB := make(map[rune]bool)
+		for _, char := range a {
+			seenA[char] = true
+		}
+		for _, char := range b {
+			seenB[char] = true
+		}
+		for _, char := range c {
+			if v1, v2 := seenA[char], seenB[char]; v1 && v2 {
+				total += getPrio(char)
+				break
+			}
+		}
+	}
+
+	fmt.Println("Part2:")
+	fmt.Println(total)
+}
+
+func getPrio(common rune) int {
+	if common >= 97 {
+		return int(common) - 96
+	} else if common >= 65 {
+		return int(common) - 38
+	}
+	return 0
+}

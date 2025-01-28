757b3b96da429bec0629739b884374f64b810d68
Flemming Mahler <flemming@mahler.io>
2025-01-28 17:42:18 +0100

    Solution for day 7 completed.

--- 2018/07/main.go
+++ 2018/07/main.go
@@ -1,57 +1,148 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"sort"
-	"strings"
+	"os"
+)
+
+const (
+	numWorkers = 5
+	baseTime   = 60
 )
 
 func main() {
-
-	data, err := os.ReadFIle("puzzle.txt")
+	// Read input file
+	file, err := os.Open("input.txt")
 	if err != nil {
-		log.Fatal("File reading error", err)
-		return
+		panic(err)
+	}
+	defer file.Close()
+
+	var order []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		order = append(order, scanner.Text())
 	}
 
-	fileContent := strings.Split(string(data), "\n")
-	fmt.Println()
-	fmt.Println("2018")
-	fmt.Println("DAY07, Part 1: The Sum of Its Parts")
-
-	var work []string
-	var current string
-
-	instructions := make(map[string][]string)
-	var complete []string
-	//var order []string
-
-	for _, fileLine := range fileContent {
-		s := strings.Split(fileLine, " ")
-		instructions[s[7]] = append(instructions[s[7]], s[1])
-		if _, ok := instructions[s[1]]; !ok {
-			instructions[s[1]] = make([]string, 0)
+	// Part 1
+	required := make([][26]int, 26)
+	for _, line := range order {
+		step := int(line[36]) - 'A'
+		prereq := int(line[5]) - 'A'
+		// Find first empty slot
+		for i := 0; i < 26; i++ {
+			if required[step][i] == 0 {
+				required[step][i] = prereq + 1 // +1 to distinguish from empty slots
+				break
+			}
 		}
 	}
 
-	for len(instructions) > 0 {
-		for k, v := range instructions {
-			for count, i := range v {
-				if i == string(current) {
-					instructions[k] = append(instructions[k][:count], instructions[k][count+1:]...)
+	part1 := ""
+	for len(part1) < 26 {
+	outer:
+		for i := 0; i < 26; i++ {
+			// Check if this letter has any requirements
+			for _, req := range required[i] {
+				if req != 0 {
+					continue outer
 				}
 			}
-			if len(instructions[k]) == 0 {
-				work = append(work, k)
+			// Found next letter
+			part1 += string('A' + i)
+			// Mark as completed
+			for j := 0; j < 26; j++ {
+				for k := 0; k < 26; k++ {
+					if required[j][k] == i+1 {
+						required[j][k] = 0
+					}
+				}
+			}
+			required[i] = [26]int{9999} // Mark as done
+			break
+		}
+	}
+	fmt.Println("Part 1: In what order should the steps in your instructions be completed?")
+	fmt.Println(part1)
+
+	// Part 2
+	required = make([][26]int, 26)
+	for _, line := range order {
+		step := int(line[36]) - 'A'
+		prereq := int(line[5]) - 'A'
+		for i := 0; i < 26; i++ {
+			if required[step][i] == 0 {
+				required[step][i] = prereq + 1
+				break
 			}
 		}
-		sort.Strings(work)
-		current = work[0]
-		delete(instructions, current)
-		complete = append(complete, work[0])
-		work = nil
 	}
-	fmt.Println("In what order should the steps in your instructions be completed?")
-	fmt.Println(strings.Join(complete, ""))
+
+	workers := make([]int, numWorkers)
+	time := make([]int, 26)
+	for i := range time {
+		time[i] = baseTime + i + 1
+	}
+	done := make([]bool, 26)
+	part2 := 0
+
+	for {
+		// Check if all tasks are done
+		allDone := true
+		for _, d := range done {
+			if !d {
+				allDone = false
+				break
+			}
+		}
+		if allDone {
+			break
+		}
+
+		// Assign available workers
+		for i := range workers {
+			if workers[i] == 0 {
+			workerLoop:
+				for j := 0; j < 26; j++ {
+					// Check if this task is available
+					for _, req := range required[j] {
+						if req != 0 {
+							continue workerLoop
+						}
+					}
+					if !done[j] && time[j] > 0 {
+						workers[i] = j + 1 // +1 to distinguish from unassigned
+						required[j] = [26]int{9999}
+						break
+					}
+				}
+			}
+		}
+
+		// Process work
+		for i := range workers {
+			if workers[i] != 0 {
+				task := workers[i] - 1
+				time[task]--
+				if time[task] == 0 {
+					done[task] = true
+					// Clear requirements
+					for j := 0; j < 26; j++ {
+						for k := 0; k < 26; k++ {
+							if required[j][k] == task+1 {
+								required[j][k] = 0
+							}
+						}
+					}
+					workers[i] = 0
+				}
+			}
+		}
+		part2++
+	}
+	fmt.Println()
+	fmt.Println("Part 2: With 5 workers and the 60+ second step durations described above,")
+	fmt.Println(" how long will it take to complete all of the steps?")
+	fmt.Println(part2)
 }

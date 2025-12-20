715e6d3b9a205cc637c989120e574cb5ccf8e4b2
Flemming Mahler <flemming@mahler.io>
2025-12-20 17:03:42 +0100

    added task description.

--- 2025/02/main.go
+++ 2025/02/main.go
@@ -23,29 +23,29 @@ func rep1(n int) []int {
 
 func buildRep() map[int]map[int]bool {
 	rep := make(map[int]map[int]bool)
-	
+
 	rep[1] = make(map[int]bool)
-	
+
 	rep[2] = make(map[int]bool)
 	for c := 1; c <= 9; c++ {
 		rep[2][c*10+c] = true
 	}
-	
+
 	rep[3] = make(map[int]bool)
 	for _, v := range rep1(3) {
 		rep[3][v] = true
 	}
-	
+
 	rep[4] = make(map[int]bool)
 	for r := 10; r < 100; r++ {
 		rep[4][r*100+r] = true
 	}
-	
+
 	rep[5] = make(map[int]bool)
 	for _, v := range rep1(5) {
 		rep[5][v] = true
 	}
-	
+
 	rep[6] = make(map[int]bool)
 	for r := 10; r < 100; r++ {
 		rep[6][r*10000+r*100+r] = true
@@ -53,22 +53,22 @@ func buildRep() map[int]map[int]bool {
 	for r := 100; r < 1000; r++ {
 		rep[6][r*1000+r] = true
 	}
-	
+
 	rep[7] = make(map[int]bool)
 	for _, v := range rep1(7) {
 		rep[7][v] = true
 	}
-	
+
 	rep[8] = make(map[int]bool)
 	for r := 1000; r < 10000; r++ {
 		rep[8][r*10000+r] = true
 	}
-	
+
 	rep[9] = make(map[int]bool)
 	for r := 100; r < 1000; r++ {
 		rep[9][r*1000000+r*1000+r] = true
 	}
-	
+
 	rep[10] = make(map[int]bool)
 	for r := 10; r < 100; r++ {
 		rep[10][r*100000000+r*1000000+r*10000+r*100+r] = true
@@ -76,7 +76,7 @@ func buildRep() map[int]map[int]bool {
 	for r := 10000; r < 100000; r++ {
 		rep[10][r*100000+r] = true
 	}
-	
+
 	return rep
 }
 
@@ -91,7 +91,7 @@ func checkRange(s string, rep map[int]map[int]bool) ([]int, []int) {
 	parts := strings.Split(s, "-")
 	a, _ := strconv.Atoi(parts[0])
 	b, _ := strconv.Atoi(parts[1])
-	
+
 	// Union of rep sets
 	rs := make(map[int]bool)
 	for k := range rep[numDigits(a)] {
@@ -100,7 +100,7 @@ func checkRange(s string, rep map[int]map[int]bool) ([]int, []int) {
 	for k := range rep[numDigits(b)] {
 		rs[k] = true
 	}
-	
+
 	// Filter to range [a, b]
 	all := make([]int, 0)
 	for r := range rs {
@@ -108,7 +108,7 @@ func checkRange(s string, rep map[int]map[int]bool) ([]int, []int) {
 			all = append(all, r)
 		}
 	}
-	
+
 	// Filter even (palindrome check)
 	even := make([]int, 0)
 	for _, r := range all {
@@ -118,7 +118,7 @@ func checkRange(s string, rep map[int]map[int]bool) ([]int, []int) {
 			even = append(even, r)
 		}
 	}
-	
+
 	return even, all
 }
 
@@ -132,14 +132,14 @@ func sum(nums []int) int {
 
 func main() {
 	rep := buildRep()
-	
+
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
 		os.Exit(1)
 	}
 	defer file.Close()
-	
+
 	scanner := bufio.NewScanner(file)
 	var input string
 	if scanner.Scan() {
@@ -147,13 +147,16 @@ func main() {
 	}
 	input = strings.TrimSpace(input)
 	ranges := strings.Split(input, ",")
-	
+
 	p1, p2 := 0, 0
 	for _, r := range ranges {
 		even, all := checkRange(r, rep)
 		p1 += sum(even)
 		p2 += sum(all)
 	}
-	
-	fmt.Println(p1, p2)
+	fmt.Println("What do you get if you add up all of the invalid IDs?")
+	fmt.Println()
+	fmt.Println(p1)
+	fmt.Println("What do you get if you add up all of the invalid IDs using these new rules?")
+	fmt.Println(p2)
 }

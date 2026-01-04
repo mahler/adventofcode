8cf5e8d00597abea3a7c9dd154846f7bc502b939
Flemming Mahler <flemming@mahler.io>
2026-01-04 15:18:18 +0100

    output adjustments.

--- 2025/04/main.go
+++ 2025/04/main.go
@@ -1,88 +1,91 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 type Point struct {
-        x, y int
+	x, y int
 }
 
 func main() {
-        file, err := os.Open("input.txt")
-        if err != nil {
-                fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
-                os.Exit(1)
-        }
-        defer file.Close()
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-        // Read warehouse and find rolls
-        rolls := make(map[Point]bool)
-        scanner := bufio.NewScanner(file)
-        y := 0
-        for scanner.Scan() {
-                line := scanner.Text()
-                for x, bin := range line {
-                        if bin == '@' {
-                                rolls[Point{x, y}] = true
-                        }
-                }
-                y++
-        }
+	// Read warehouse and find rolls
+	rolls := make(map[Point]bool)
+	scanner := bufio.NewScanner(file)
+	y := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for x, bin := range line {
+			if bin == '@' {
+				rolls[Point{x, y}] = true
+			}
+		}
+		y++
+	}
 
-        if err := scanner.Err(); err != nil {
-                fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-                os.Exit(1)
-        }
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
 
-        adjacents := []Point{
-                {-1, -1}, {0, -1}, {1, -1},
-                {-1, 0}, {1, 0},
-                {-1, 1}, {0, 1}, {1, 1},
-        }
+	adjacents := []Point{
+		{-1, -1}, {0, -1}, {1, -1},
+		{-1, 0}, {1, 0},
+		{-1, 1}, {0, 1}, {1, 1},
+	}
 
-        a1, a2, round := 0, 0, 0
-        elvesWorking := true
-        removed := make(map[Point]bool)
+	a1, a2, round := 0, 0, 0
+	elvesWorking := true
+	removed := make(map[Point]bool)
 
-        for elvesWorking {
-                round++
-                for roll := range rolls {
-                        adjacentRolls := 0
-                        for _, adj := range adjacents {
-                                neighbor := Point{roll.x + adj.x, roll.y + adj.y}
-                                if rolls[neighbor] {
-                                        adjacentRolls++
-                                }
-                                if adjacentRolls > 3 {
-                                        break
-                                }
-                        }
-                        if adjacentRolls < 4 {
-                                removed[roll] = true
-                        }
-                }
+	for elvesWorking {
+		round++
+		for roll := range rolls {
+			adjacentRolls := 0
+			for _, adj := range adjacents {
+				neighbor := Point{roll.x + adj.x, roll.y + adj.y}
+				if rolls[neighbor] {
+					adjacentRolls++
+				}
+				if adjacentRolls > 3 {
+					break
+				}
+			}
+			if adjacentRolls < 4 {
+				removed[roll] = true
+			}
+		}
 
-                if len(removed) > 0 {
-                        // Remove from rolls
-                        for r := range removed {
-                                delete(rolls, r)
-                        }
+		if len(removed) > 0 {
+			// Remove from rolls
+			for r := range removed {
+				delete(rolls, r)
+			}
 
-                        if round == 1 {
-                                a1 += len(removed)
-                        }
-                        a2 += len(removed)
+			if round == 1 {
+				a1 += len(removed)
+			}
+			a2 += len(removed)
 
-                        // Clear removed set
-                        removed = make(map[Point]bool)
-                } else {
-                        elvesWorking = false
-                }
-        }
+			// Clear removed set
+			removed = make(map[Point]bool)
+		} else {
+			elvesWorking = false
+		}
+	}
 
-        fmt.Printf("Part one: %d rolls removed\n", a1)
-        fmt.Printf("Part two: %d rolls removed in %d rounds of work\n", a2, round)
+	fmt.Println("Part one: Consider your complete diagram of the paper roll locations. How many rolls of paper can be accessed by a forklift?")
+	fmt.Println(a1)
+	fmt.Println()
+	fmt.Println("Part two: How many rolls of paper in total can be removed by the Elves and their forklifts?")
+	fmt.Println(a2)
 }
